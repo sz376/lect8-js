ab2b87b5bc865d0ed63d07f3876b4cea90215bdb
EC2 Default User <ec2-user@ip-172-31-33-20.ec2.internal>
2020-09-29 21:17:50 +0000

    Added a button that shows an alert on click

--- static/hello.js
+++ static/hello.js
@@ -1,2 +1,8 @@
 // Everything in between these tags is Javascript!
 alert("Hello, world!");
+
+window.onload = function(){
+    document.getElementById("submit").onclick = function(){
+    alert("I submitted!");
+    };
+}
